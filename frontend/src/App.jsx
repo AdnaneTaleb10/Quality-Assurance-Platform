effec7b072cf774e9c7184dacf740edffccba9be
hocineguerid01 <hasnou.guerid01@gmail.com>
2026-04-19 12:10:42 +0100

    add authentication without integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/" element={<Landing />} />
           <Route path="/" element={<Landing />} />
         </Routes>
       </BrowserRouter>
